3abe9b13d549aeb49762b44db8a08ef10b2dd308
root <root@Chris.localdomain>
2025-08-15 03:54:51 +0930

    pretesting input data conversion

--- Mnist_numberAI/Mnist_input_translator.cpp
+++ Mnist_numberAI/Mnist_input_translator.cpp
@@ -5,14 +5,23 @@
 
 using namespace std;
 
+/*
+    TESTING
+    - check arrays are correctly converted and configured
+    - check labels match images
+
+*/
+
+
+
+
 //so we want to convert the training images from the mnist dataset into
 //an array of images, these images are 784 pixels 28x28, with colours from 0-255
-// 
+// we want to have an array with images with colour ranges from 0-1 and labels which hold the value the images shows
 
 
 
-vector<int> convert_image_data(string file_name){
-    
+vector<float> convert_image_data(string file_name){
 
     // read file from file name
     ifstream file(file_name, ios::binary);
@@ -26,7 +35,9 @@ vector<int> convert_image_data(string file_name){
         if (count > 15){
             raw_data.push_back(static_cast<float>(byte)/255);
         }
-        count++;
+        else{
+            count++;
+        }
     }
 
     // raw data now contains pixels from 60000 images, with images being seperated every 784 pixels
@@ -35,16 +46,48 @@ vector<int> convert_image_data(string file_name){
     return raw_data;
 }
 
+// creates an array with the images labels, for example label for image 1 is label[0]. should be between 0-9
+vector<int> convert_label_data(string file_name){
 
+    ifstream file(file_name, ios::binary);
+    unsigned char byte;
+    int count = 0;
+    vector<int> raw_data;
+
+    while (file.read(reinterpret_cast<char*>(&byte), 1)) {
+        if (count > 15){
+            raw_data.push_back(static_cast<int>(byte));
+        }
+        else{
+            count++;
+        }
+    }
+
+    return raw_data;
+}
 
 
 int main(){
-
+    // intialise all data containers
+    vector<float> train_data;
+    vector<float> test_data;
+    vector<int> train_labels;
+    vector<int> test_labels;
+    // the file paths for the data
     string train_images_file_name ="Mnist_data/Train_data/train-images.idx3-ubyte"; 
-    string test_images_file_name ="Mnist_data/Test_data/t10k-images.idx1-ubyte";
+    string test_images_file_name ="Mnist_data/Test_data/t10k-images.idx3-ubyte";
+    string train_labels_file_name ="Mnist_data/Train_data/train-labels.idx1-ubyte"; 
+    string test_labels_file_name ="Mnist_data/Test_data/t10k-labels.idx1-ubyte";
+
 
     train_data = convert_image_data(train_images_file_name);
     test_data = convert_image_data(test_images_file_name);
+    train_labels = convert_label_data(train_labels_file_name);
+    test_labels = convert_label_data(test_labels_file_name);
+
+
+
+
 
 
 
